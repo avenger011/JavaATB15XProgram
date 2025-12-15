89a4bee999cd5686f5e5cc8ad4debf331e8b77a1
ganesh <saiganeshrao01@gmail.com>
2025-12-15 17:44:51 +0530

    practice questions1

--- src/ex_01_javabasics/Lab003.java
+++ src/ex_01_javabasics/Lab003.java
@@ -2,23 +2,22 @@ package ex_01_javabasics;
 /// fibbonacii of the number
 public class Lab003 {
  public static void main(String[] args){
-int num=10;
-int a=0;
-int b=1;
 
-if(num>=0)
-{System.out.println(a);}
-if(num>=1)
-{System.out.println(b);
+ int a=0;
+ int b=1;
+ int num=10;
+ int c=0;
+ if(num>=a){System.out.println(a);}
+ if(num>=b){System.out.println(b);}
+ if(num>b){
+ for(int i=2;i<=num;i++)
+ {
+     c=a+b;
+     System.out.println(c);
+     a=b;
+     b=c;
 
-for(int i=1;i<num;i++)
-    {System.out.println(a+b);
-        a=b;
-        b=i;
-
-    }
-}
-
+ }}
 
 
 
