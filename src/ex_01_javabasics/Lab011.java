1fbe890fdd73d25d6fc39515808d423960bcfd4a
ganesh <saiganeshrao01@gmail.com>
2026-01-05 07:25:31 +0530

    practice questions1

---
+++ src/ex_01_javabasics/Lab011.java
@@ -0,0 +1,22 @@
+package ex_01_javabasics;
+
+import java.util.Scanner;
+
+public class Lab011 {
+    public static void main( String[] args){
+        Scanner sc=new Scanner(System.in);
+System.out.println("enter the  umber");
+int num=sc.nextInt();
+ switch (num) {
+     case 1:
+         System.out.println(num);
+         break;
+     case 2:
+         System.out.println(num);
+         break;
+     default:
+         System.out.println("defauly");
+         break;
+
+ }  }
+}
