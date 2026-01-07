408346804fe7e8ef15938cc3572d55c867d46816
ganesh <saiganeshrao01@gmail.com>
2026-01-07 07:48:57 +0530

    practice questions1

--- src/ex_01_javabasics/Lab12.java
+++ src/ex_01_javabasics/Lab12.java
@@ -1,14 +1,19 @@
 package ex_01_javabasics;
-
+import java.util.Scanner;
 public class Lab12 {
     //to find factorial of 5
+    //factprial of -numbers is 1
+    //factorial of 0 =1
     public static void main(String[] args){
         int a=0;
         int b=1;
-        int num=5;
+
+        Scanner sc=new Scanner(System.in);
+        int num=sc.nextInt();
         int fact=1;
 
-        for(int i=num;i>=1;i--) {
+
+        for(int i=num;i>=1;i--) {              //in factorial always use revcerse approach i.e --i coz it handels the -ve number case;
             fact=fact*i;
             System.out.println(i);
         }
