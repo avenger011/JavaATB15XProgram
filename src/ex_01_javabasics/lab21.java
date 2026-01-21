59be74744cc1a1afc70b378a69d74304effdcd44
ganesh <saiganeshrao01@gmail.com>
2026-01-21 23:49:06 +0530

    GAM

--- src/ex_01_javabasics/lab21.java
+++ src/ex_01_javabasics/lab21.java
@@ -2,19 +2,14 @@ package ex_01_javabasics;
 
 public class lab21 {
     public static void main(String[] args){
-        int n=5;
-        String[][] a=null;
+        int n=6;
+      for(int i=0;i<n;i++)
+      {
 
-        for(int i=0;i<n;i++ )
-        {
-            for (int j=0;j<n;j++)
-            {
-              if(j<i)
-              {a[i][j]="*";}
-              else{
-                  a[i][j]=" ";
-              }
-            }
-        }
+          for(int j=0;j<2*i-1;j++)System.out.print(" ");
+          for(int k=n;k>2*i-1;k--)System.out.print("*");
+
+          System.out.println();
+      }
     }
 }
